bd6fd1329c257c2a86b97b54e7405ddade3cae56
Dave Dosanjh <6342618+ddosanjh@users.noreply.github.com>
2020-10-31 08:38:53 -0700

    save

---
+++ index.js
@@ -0,0 +1,1 @@
+export {registerAutoCrudStoreModule} from './src/autocrud-store-module'
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: [
+     "js",
+     "json",
+     "vue"
+  //   "jsx",
+  //   "ts",
+  //   "tsx",
+  //   "node"
+  ],
+};
